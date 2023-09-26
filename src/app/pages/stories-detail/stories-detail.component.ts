26c7ae5922810a053e7fac7269a24148cc438e1f
Boris Novi <borisnovikov4815@gmail.com>
2023-09-26 14:38:26 +0400

    feat: change single story structure

--- src/app/pages/stories-detail/stories-detail.component.ts
+++ src/app/pages/stories-detail/stories-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NewsService } from '../../services/news.service';
 import { singleStory } from 'src/app/interfaces/stories-interface';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-stories-detail',
@@ -10,14 +11,43 @@ import { singleStory } from 'src/app/interfaces/stories-interface';
 })
 export class StoriesDetailComponent implements OnInit {
   single_story: singleStory;
+  author: string;
+  category: string;
+  date: string;
+  header: string;
+  id: number;
+  images_0: string[];
+  images_1: string[];
+  main_image: string;
+  main_text: string;
+  sub_text_0: string;
+  sub_text_1: string;
+  subheader: string;
+  video: SafeResourceUrl;
 
-  constructor(private route: ActivatedRoute, private newsService: NewsService) { }
+  image_0_0: string; // Если будешь делать карусель, то убери это
+  image_1_0: string; // Если будешь делать карусель, то убери это
+
+  constructor(private route: ActivatedRoute, private newsService: NewsService, private sanitizer: DomSanitizer) { }
 
   async ngOnInit(): Promise<void> {
     const storiesId = this.route.snapshot.params['id'].slice(3);
 
     (await this.newsService.getStoriesById(storiesId)).subscribe(data => {
       this.single_story = data;
+      this.author = data.author;
+      this.category = data.category;
+      this.date = data.date;
+      this.header = data.header;
+      this.id = data.id;
+      this.images_0 = data.images_0;
+      this.images_1 = data.images_1;
+      this.main_image = data.main_image;
+      this.main_text = data.main_text;
+      this.sub_text_0 = data.sub_text_0;
+      this.sub_text_1 = data.sub_text_1;
+      this.subheader = data.subheader;
+      this.video = this.sanitizer.bypassSecurityTrustResourceUrl(data.video);
     });
   }
 }
